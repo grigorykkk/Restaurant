c928c031ac11d64d52c42cf6afe49ac184124818
grigory <gkilich5@gmail.com>
2025-11-10 09:46:28 +0300

    fix

--- obj/Debug/net9.0/Restaurant site.AssemblyInfo.cs
+++ obj/Debug/net9.0/Restaurant site.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Restaurant_site")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+910b0137a81509e7ecadfa878fab0ab15d8a6cfb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+697f2f9074ce617f8d4781d8c0da6bdbc6ef1da7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Restaurant_site")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Restaurant_site")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
